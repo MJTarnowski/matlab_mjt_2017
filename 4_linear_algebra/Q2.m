0ae131e74413b3922fde34229b34b920a8b372c7
MJTarnowski <matthew.tarnowski1@gmail.com>
2017-11-16 12:30:05 +0000

    started working on worksheet 4

---
+++ 4_linear_algebra/Q2.m
@@ -0,0 +1,13 @@
+%% Q2 from linear algebra worksheet
+
+%create a square matrix of random numbers using randn(dimension)
+A = randn(512);
+% matrix b is of dimensions (512 x 1) ie one column of 512 random numbers
+b = randn(512,1);
+c = randn(2,3);
+
+%
+length(A)
+length(b)
+length(c)
+
