09838e9d65ed9384eed0e652b184fd3d4d22f31c
Matthew Tarnowski <matthew.tarnowski1@gmail.com>
2017-11-17 10:43:41 +0000

    added notes from lecture 5

---
+++ 5_sw_carpentry/lecture_5_notes.m
@@ -0,0 +1,49 @@
+%% sw carpentry - lecture notes
+
+% key takeaway: be consistent
+% anything you can't communicate with code, you communicate with commments
+
+% function has to have same name as the file - put main one on the top
+% you can then have sub/nested functions 
+% however these sub/nested functions are only accessible within the file
+% rather than outside the file
+
+% to access a specific function from command line, it needs to be saved in
+% its own file of same name
+
+
+%classes do exist in matlab but aren't used that much
+
+% if subfunctions are in separate files they can still be accessed from a
+% function in a different file
+
+% you dont really need end for sub functions, just required for nested
+
+%nested functions require ends
+% you can think of nested functions like lambda functions (python) or
+% anonymous functions (also in matlab)
+% nested functions can access parent variables without them needing to be
+% accessed by each function in its arguments (like subfunctions)
+
+%% GUIs in MATLAB
+
+% GUI might be useful for demo of your group project in the final week
+% use global vbles with caution because if they are changed many places
+% within your code because it's hard to see what's happening to the global
+% variable
+
+%% projects
+
+% -genetic osc - 2D ode - guyantes & poyatos
+% -image reconstruction
+% -ODE models of infection
+% -cancer spatial modelling - cellular automata, continuum PDE model -
+% matrices / DE
+% - data analysis of flocking behaviour - starlings - paper + dataset -
+% analyse the velocity of the flock or visualisation or model
+
+% ~ 7 groups &~ 5 projects
+
+% - name of group and whihc proj/paper you're working on 
+    % email to martin.robinson@cs.ox.ac.uk
+    
